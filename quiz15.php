28b7d38f2a3a684edff92874150fb95119f36e14
Rb Denzel Mistal <120579209+arb3ng@users.noreply.github.com>
2023-05-24 19:00:53 +0800

    Update quiz15.php (Added Session Variable)
    Added session variable to indicate that the user has submitted the quiz

--- quiz15.php
+++ quiz15.php
@@ -29,9 +29,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         session_start();
         $_SESSION['score15'] = $score15;
 
-        // Redirect to the index.php file
-        header('Location: index.php');
-        exit;
+        // Set a session variable to indicate that the user has submitted the quiz
+        $_SESSION['quiz15_submitted'] = true;
+        // Redirect back to index.php
+        header("Location: index.php");
+        exit();
     } else {
         $errorMessage = "Please answer all questions before submitting.";
     }
